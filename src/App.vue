faddb20b63e83e21b9877e57853bb90a9fc1f526
Alun Price <alunprice089@gmail.com>
2023-01-28 14:37:07 +0000

    s7 73 added zoom css transition and animation/transition controls

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello World!</h2>
     <h2 v-else key="secondary">Another hello!</h2>
+  </transition>
+  
+   -->
+
+  <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -11,7 +17,7 @@ export default {
   name: "App",
   data() {
     return {
-      flag: false,
+      flag: true,
     };
   },
 };
@@ -28,6 +34,49 @@ export default {
 
 .fade-leave-to {
   transition: all 2.5s linear;
+  opacity: 0;
+}
+
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+
+  to {
+    transform: scale(0, 0);
+  }
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 1s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+.zoom-enter-to {
   opacity: 0;
 }
 </style>
